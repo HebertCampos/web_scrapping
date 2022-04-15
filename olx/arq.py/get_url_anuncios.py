955f294864746c7bfad8856fc9a0f82525dc6f09
Hebert Campos <hebertcdev@gmail.com>
2022-04-14 23:19:50 -0300

    conectando a pagina da olx

---
+++ olx/arq.py/get_url_anuncios.py
@@ -0,0 +1,22 @@
+from urllib.request import urlopen, Request
+from urllib.error import URLError, HTTPError
+from bs4 import BeautifulSoup
+
+def url():
+    url = "https://al.olx.com.br/alagoas/autos-e-pecas/motos"
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'}
+    url_headers = [url,headers]
+    return url_headers
+
+def conexaoSoup():
+    urL = url()
+    try:
+        req = Request(urL[0], headers= urL[1])
+        response = urlopen(req)
+        html = response.read()
+    except HTTPError as e:
+        print(e.status, e.reason)
+    except URLerror as e:
+        print(e.reason)
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup
