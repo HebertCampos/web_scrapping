d2ef61e1ef667e0cd37157b3f03e8be703ba6f75
Hebert Campos <hebertcdev@gmail.com>
2022-04-16 14:26:04 -0300

    lendo arquivos da pagina olx

--- olx/arq.py/main.py
+++ olx/arq.py/main.py
@@ -3,9 +3,8 @@ from get_info_anuncios import *
 
 url = "https://al.olx.com.br/alagoas/autos-e-pecas/motos"  
 soup = conexaoSoup(url)
-# for i in range(3):
-#     time.sleep(5)
-#     print(getHrefs(soup))
-#     print(nextPage())
-#     soup = conexaoSoup(nextPage())
-# print(getHrefs(soup))
+for i in range(3):
+    time.sleep(10)
+    print(getHrefs(soup))
+    print(nextPage(soup))
+    soup = conexaoSoup(nextPage(soup))
