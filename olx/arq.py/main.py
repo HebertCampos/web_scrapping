726b16e12f37751d5500d40ed7ec4f7e22f449ea
hebert <hebertcdev@gmail.com>
2022-05-12 13:21:44 -0300

    inserindo dados coletados no bd

--- olx/arq.py/main.py
+++ olx/arq.py/main.py
@@ -1,13 +1,32 @@
 from get_url_anuncios import *
 from get_info_anuncios import *
 
-url = "https://al.olx.com.br/alagoas/autos-e-pecas/motos" 
+import sqlite3
 
-soup = conexaoSoup(url)
-a = []
-for i in range(60):
-    hrefs = getHrefs(soup)
-    for i in range(len(hrefs)):
-        soup1 = conexaoSoup(hrefs[i])
-        a.append(anuncio(soup1, hrefs[i]))
-    soup = conexaoSoup(nextPage(soup))
+con = sqlite3.connect(r'olx_motos.db')
+
+cur = con.cursor()
+
+
+def getAnuncios():
+    url = "https://al.olx.com.br/alagoas/autos-e-pecas/motos" 
+    soup = conexaoSoup(url)
+    a = []
+    for i in range(1):
+        hrefs = getHrefs(soup)
+        for i in range(len(hrefs)):
+            b = []
+            soup1 = conexaoSoup(hrefs[i])
+            b.append(anuncio(soup1, hrefs[i]))
+            if(len(b[0])==12):
+                a.append(b[0])
+        soup = conexaoSoup(nextPage(soup))
+    return a
+
+
+sql_insert = 'insert into anuncios values (?,?,?,?,?,?,?,?,?,?,?,?)'
+recset = getAnuncios()
+for rec in recset:
+    cur.execute(sql_insert, rec)
+con.commit()
+con.close()
